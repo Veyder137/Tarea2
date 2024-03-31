cb4841cc68b3c2a96343d5d506d7f441818d061a
Veyder137 <juancholar.lar@gmail.com>
2024-03-31 13:27:36 -0300

    Comienzo con coleccionPieza

--- tarea2/src/coleccionPiezas.cpp
+++ tarea2/src/coleccionPiezas.cpp
@@ -1,7 +1,8 @@
 #include "../include/coleccionPiezas.h"
 
 struct rep_coleccionpiezas{
-    
+    TPieza pieza;
+    TColeccionPiezas * sig;
 };
 
 TColeccionPiezas crearColeccionPiezasVacia(){ 
@@ -9,11 +10,30 @@ TColeccionPiezas crearColeccionPiezasVacia(){
 }
 
 void insertarPiezaColeccionPiezas(TColeccionPiezas &coleccionPiezas, TPieza pieza){
-    
+    TColeccionPiezas nuevoNodo = new rep_coleccionpiezas;
+    nuevoNodo -> pieza = pieza;
+    nuevoNodo -> sig = NULL;
+
+    if (coleccionPiezas == NULL || idTPieza(pieza) < idTPieza(coleccionPiezas->pieza)) {
+        *nuevoNodo -> sig = coleccionPiezas;
+        coleccionPiezas = nuevoNodo;
+    }
+    else {
+        TColeccionPiezas actual = coleccionPiezas;
+        while (actual -> sig != NULL && idTPieza(pieza) < idTPieza(coleccionPiezas->pieza)){
+            actual = *actual -> sig;
+        }
+        nuevoNodo -> sig = actual -> sig;
+        *actual -> sig = nuevoNodo;
+    }
 }
 
 void imprimirColeccionPiezas(TColeccionPiezas coleccionPiezas){
-    
+    TColeccionPiezas actual = coleccionPiezas;
+    while (actual != NULL) {
+        imprimirTPieza(actual -> pieza);
+        actual = *actual -> sig;
+    }
 }
 
 bool esVaciaColeccionPiezas(TColeccionPiezas piezas){
@@ -33,5 +53,12 @@ void removerPiezaColeccionPiezas(TColeccionPiezas &coleccionPiezas, int idPieza)
 }
 
 void liberarColeccionPiezas(TColeccionPiezas &coleccionPiezas){
-    
+    TColeccionPiezas actual = coleccionPiezas;
+    while (actual != NULL) {
+        TColeccionPiezas siguiente = *actual -> sig;
+        liberarTPieza(actual -> pieza);
+        delete actual;
+        actual = siguiente;
+    }
+    coleccionPiezas = NULL;
 }
