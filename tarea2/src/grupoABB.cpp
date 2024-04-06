8139da90f07782ca581bd0afb5620cea3e5a2bac
Veyder137 <juancholar.lar@gmail.com>
2024-04-06 16:12:34 -0300

    avances grupoABB

--- tarea2/src/grupoABB.cpp
+++ tarea2/src/grupoABB.cpp
@@ -11,25 +11,43 @@ TGrupoABB crearTGrupoABBVacio(){
     return Arbolito;
 }
 
-
-
 void insertarTVisitanteTGrupoABB(TGrupoABB &grupoABB, TVisitante visitante){
-    TGrupoABB hoja = new rep_grupoABB;
-    hoja -> vis = visitante;
-    hoja -> key = idTVisitante(visitante);
-    hoja -> izq = hoja -> der =NULL;
-
+    TGrupoABB nuevoNodo = new rep_grupoABB;
+    nuevoNodo -> vis = visitante;
+    nuevoNodo -> key = idTVisitante(visitante);
+    nuevoNodo -> izq = nuevoNodo -> der = NULL;
+    
     if (grupoABB == NULL)
-        grupoABB = hoja;
-    else 
-        if (idTVisitante(visitante) < grupoABB -> key)
-            insertarTVisitanteTGrupoABB (grupoABB -> izq, visitante);
-        else if (idTVisitante(visitante) > grupoABB -> key)
-            insertarTVisitanteTGrupoABB (grupoABB -> der, visitante);
+        grupoABB = nuevoNodo;
+    else {
+        TGrupoABB actual = grupoABB;
+        while (true) {
+            if (idTVisitante(visitante) < idTVisitante(actual -> vis))
+                if (actual -> vis == NULL){
+                    actual -> izq = nuevoNodo;
+                    break;
+                }
+                else
+                    actual = actual -> izq;
+            else
+                if (actual -> der ==NULL) {
+                    actual -> der = nuevoNodo;
+                    break;
+                }
+                else
+                    actual = actual -> der;
+        }
+        
+    }
+
 }
 
 void imprimirTGrupoABB(TGrupoABB grupoABB){
-    
+    if (grupoABB != NULL) {
+        imprimirTGrupoABB(grupoABB -> izq);
+        imprimirTVisitante(grupoABB -> vis);
+        imprimirTGrupoABB (grupoABB -> der);
+    }
 }
 
 bool existeTVisitanteTGrupoABB(TGrupoABB grupoABB, int idVisitante){
@@ -52,48 +70,80 @@ TVisitante obtenerTVisitanteTGrupoABB(TGrupoABB grupoABB, int idVisitante){
     return grupoABB -> vis;
 }
 
-TGrupoABB minimo (TGrupoABB grupoABB){
-    if (grupoABB == NULL) 
-        return NULL;
-    else {
-        while(grupoABB -> izq != NULL){
-            grupoABB = grupoABB -> izq;
-        }
-    return grupoABB;
-    }
-}
-
 void removerTVisitanteTGrupoABB(TGrupoABB &grupoABB, int idVisitante){
-    TGrupoABB gABB = grupoABB;
-    if (gABB != NULL) {
-        if ( idVisitante < gABB -> key)
-            removerTVisitanteTGrupoABB(gABB, idVisitante);
-        else if (idVisitante > gABB -> key)
-            removerTVisitanteTGrupoABB(gABB, idVisitante);
-        else { // idVisitante == gABB -> key
-            if (gABB -> der == NULL){
-                TGrupoABB aBorrar = gABB;
-                gABB = gABB -> izq;
-                delete aBorrar;
-            }
-            else {
-                TGrupoABB min_t_der = minimo(gABB -> der);
-                gABB -> key = min_t_der -> key;
-                gABB -> vis = min_t_der -> vis;
-                removerTVisitanteTGrupoABB(gABB, idVisitante);
-            }
+
+    TGrupoABB reemplazo = new rep_grupoABB;
+    reemplazo -> vis = NULL;
+    reemplazo -> key = NULL;
+    reemplazo -> izq = reemplazo -> der = NULL;
+    
+    TGrupoABB padre = NULL;
+    TGrupoABB actual = grupoABB;
+
+    if (idVisitante != idTVisitante(actual -> vis)) {
+        padre = actual;
+        while (idVisitante != idTVisitante(actual -> vis)) { //buscamos el visitante a remover
+            if (idVisitante < idTVisitante(actual -> vis)) 
+                actual = actual -> izq;
+            else
+                actual -> der;
         }
     }
+
+    if (actual -> izq == NULL && actual -> der == NULL) { // Caso 1 Nodo sin hijos
+        if (padre == NULL) 
+            grupoABB = actual -> der;
+        else {
+            if (padre -> izq == actual)
+                padre -> izq = actual -> der;
+            else
+                padre -> der = actual -> der;
+        }
+        delete actual;
+    }
+    else  
+        if (actual -> izq == NULL) { // Caso 2: Nodo con un hijo derecho
+            if (padre == NULL) 
+                grupoABB = actual->der;
+            else 
+                if (padre -> izq == actual) 
+                padre -> izq = actual -> der;
+            else 
+                padre -> der = actual -> der;
+            
+            delete actual;
+        }
+        else
+            if (actual -> der == NULL) { // Caso 3: Nodo con un hijo izquierdo
+                if (padre == NULL)
+                    grupoABB -> izq;
+                else 
+                    if (padre -> izq == actual)
+                        padre -> izq = actual -> izq;
+                    else
+                        padre -> der = actual -> izq;
+            }
+            else { // Caso 3: Nodo con dos hijos
+                TGrupoABB temp = actual -> izq;
+                TGrupoABB padreTemp = actual;
+                while (temp -> der != NULL) {
+                    padreTemp = temp;
+                    temp = temp -> der;
+                }
+
+                actual -> vis = temp -> vis;
+
+                if (padreTemp -> izq == temp)
+                    padreTemp -> izq = temp -> izq;
+                else
+                    padreTemp -> der = temp -> izq;
+
+                delete temp;
+            }
 }
 
 nat alturaTGrupoABB(TGrupoABB grupoABB) {
-    if (grupoABB == NULL) 
-        return 0;
-    else {
-        int altura_izq = alturaTGrupoABB(grupoABB->izq);
-        int altura_der = alturaTGrupoABB(grupoABB->der);
-        return 1 + (altura_izq > altura_der ? altura_izq : altura_der);
-    }
+
 }
 
 int cantidadVisitantesTGrupoABB(TGrupoABB grupoABB){
@@ -109,7 +159,11 @@ void liberarTGrupoABB(TGrupoABB &grupoABB){
 }
 
 TVisitante maxIdTVisitanteTGrupoABB(TGrupoABB grupoABB){
-    return NULL;
+    while (grupoABB -> der != NULL) {
+        grupoABB = grupoABB -> der;
+    }
+    
+    return grupoABB -> vis;
 }
 
 TVisitante obtenerNesimoVisitanteTGrupoABB(TGrupoABB grupoABB, int n){
